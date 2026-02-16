9fbc6e1cc285868b094a67456ab0d9d0e9843d3e
Ahmed Metwaly <luisarabss@gmail.com>
2026-02-16 16:53:11 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,12 +1,11 @@
 
 export const firebaseConfig = {
-  "apiKey": "AIzaSyAwV9BvdaMCTXq2L2q6L6B4dYgL9yMvX_Y",
-  "authDomain": "venti-hr.firebaseapp.com",
-  "databaseURL": "https://venti-hr-default-rtdb.firebaseio.com",
-  "projectId": "venti-hr",
-  "storageBucket": "venti-hr.appspot.com",
-  "messagingSenderId": "910269639534",
-  "appId": "1:910269639534:web:06e4a2c5a1b383842c16c9",
-  "measurementId": "G-5YJ72T1L6J"
+  "apiKey": "AIzaSyD3DRq5QfE05KedmoeRfD_CTa2_H4Mw6c8",
+  "authDomain": "studio-4978085334-51a01.firebaseapp.com",
+  "databaseURL": "https://studio-4978085334-51a01-default-rtdb.firebaseio.com",
+  "projectId": "studio-4978085334-51a01",
+  "storageBucket": "studio-4978085334-51a01.appspot.com",
+  "messagingSenderId": "337969546584",
+  "appId": "1:337969546584:web:8f3671362c6bd5653a512d"
 };
     
