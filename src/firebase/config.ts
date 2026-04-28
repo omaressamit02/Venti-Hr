c17f5aeb4384364e458facfab1e7b1466105158a
omaressamit02 <omaressamit02@gmail.com>
2026-04-28 15:06:56 +0300

    Main

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,11 @@
 
 export const firebaseConfig = {
-  "apiKey": "AIzaSyD3DRq5QfE05KedmoeRfD_CTa2_H4Mw6c8",
-  "authDomain": "studio-4978085334-51a01.firebaseapp.com",
-  "databaseURL": "https://studio-4978085334-51a01-default-rtdb.firebaseio.com",
-  "projectId": "studio-4978085334-51a01",
-  "storageBucket": "studio-4978085334-51a01.appspot.com",
-  "messagingSenderId": "337969546584",
-  "appId": "1:337969546584:web:8f3671362c6bd5653a512d"
+  "apiKey": "AIzaSyDwAx0opm_D975cgWGHuPIniWTpNHSXF4c",
+  "authDomain": "venti-hr.firebaseapp.com",
+  "databaseURL": "https://venti-hr-default-rtdb.firebaseio.com",
+  "projectId": "venti-hr",
+  "storageBucket": "venti-hr.firebasestorage.app",
+  "messagingSenderId": "41957831368",
+  "appId": "1:41957831368:web:7ad3908cd87aea5171abfd"
 };
     
